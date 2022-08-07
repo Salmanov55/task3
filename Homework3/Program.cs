4b2d8c8af0c8b5e6fa31a9ed640c53bcbc04f1ff
Ali <lisalmanov84@gmail.com>
2022-08-07 19:11:01 +0400

    task3

---
+++ Homework3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Homework3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {1,2,3,4,5};
+            Console.WriteLine();
+        }
+        static void PrintMinandMax(int[] arr)
+        {
+            int[] nums = { 1, 2, 3, 4, 5 };
+            int Min = 0;
+            int Max = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+               if (nums[Min] > nums[i])
+                {
+                    Min =nums [i];
+                }
+               else if (nums[Max] < nums[i])
+                {
+                    Max =nums [i];
+                }
+                   int temp = nums[Min];
+                nums [Min] = nums [Max];
+                nums [Max] = temp;
+            }
+        }
+    }
+
+}
